a3689ca4f0e8764fae3bb68b4af50409a8fef53f
Dalex Tanguy <dalex.tanguy@gmail.com>
2018-02-14 10:24:32 +0100

    security check + phpcsfix

--- tests/Unit/SimpleTest.php
+++ tests/Unit/SimpleTest.php
@@ -1,14 +1,11 @@
 <?php
 
-
 namespace testRepo\Test\Unit;
 
 final class SimpleTest extends \PHPUnit\Framework\TestCase
 {
-
     public function testTrue()
     {
-		$this->assertTrue(true, 'This should already work.');
+        $this->assertTrue(true, 'This should already work.');
     }
-
 }
