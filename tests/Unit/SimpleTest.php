d12fa2275cf704845a7685462e2a3552e8f51808
Dalex Tanguy <dalex.tanguy@gmail.com>
2018-02-14 10:09:44 +0100

    unit test

---
+++ tests/Unit/SimpleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace testRepo\Test\Unit;
+
+final class SimpleTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testTrue()
+    {
+		$this->assertTrue(true, 'This should already work.');
+    }
+
+}
